c6f6af2c1f60124c03a3c7a598671994da2a1cbc
James Zhao <james@tokbox.com>
2016-06-15 17:15:39 -0700

    initial platform

--- SWE500/project/js/main.js
+++ SWE500/project/js/main.js
@@ -1,10 +1,19 @@
+$(document).ready(function() {
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyAn6LnSQpx5BG9YkslXQkz7SnW0rZ9val0",
-        authDomain: "project-4337251130944270718.firebaseapp.com",
+        //authDomain: "project-4337251130944270718.firebaseapp.com",
+        authDomain: "jamesz.us",
         databaseURL: "https://project-4337251130944270718.firebaseio.com",
         storageBucket: "project-4337251130944270718.appspot.com",
     };
+
+    var ref = new Firebase("https://project-4337251130944270718.firebaseio.com/");
+
+    var userName;
+    var userPic;
+    var userId;
     firebase.initializeApp(config);
 
    // This is called with the results from from FB.getLoginStatus().
@@ -35,7 +44,6 @@
     // code below.
     function checkLoginState() {
         FB.getLoginStatus(function(response) {
-            alert(response)
             statusChangeCallback(response);
         });
     }
@@ -46,7 +54,7 @@
             cookie     : true,  // enable cookies to allow the server to access
             // the session
             xfbml      : true,  // parse social plugins on this page
-            version    : 'v2.5' // use graph api version 2.5
+            version    : 'v2.6' // use graph api version 2.5
         });
 
         // Now that we've initialized the JavaScript SDK, we call
@@ -62,13 +70,29 @@
         // These three cases are handled in the callback function.
 
         FB.getLoginStatus(function(response) {
+
             statusChangeCallback(response);
         });
 
+        FB.Event.subscribe('auth.login', function() {
+            console.log("logged in");
+            if(window.location.pathname == '/project/login.html') {
+                window.location.href = 'index.html';
+            }
+        });
+
+        FB.Event.unsubscribe('auth.login', function() {
+            console.log('here');
+        });
+
+        FB.Event.subscribe('auth.logout', function(){
+            window.location.href = 'login.html';
+        });
     };
 
     // Load the SDK asynchronously
     (function(d, s, id) {
+
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
@@ -79,10 +103,59 @@
     // Here we run a very simple test of the Graph API after login is
     // successful.  See statusChangeCallback() for when this call is made.
     function testAPI() {
-        console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me', function(response) {
-            console.log('Successful login for: ' + response.name);
-            document.getElementById('status').innerHTML =
-                    'Thanks for logging in, ' + response.name + '!';
+        FB.api('/me/', function(response) {
+            console.log('Facebook Login successful: ' + response.name);
+
+            userName = response.name;
+            userId = response.id;
+            $("#user").html('<h4 class="sub-header">You are signed in as ' + userName + '</h4>');
+            writeUserData(userId, userName);
+            FB.api('/me/picture', function(response) {
+                userPic = response.data.url;
+                $('#userPic').html('<img src="' + userPic + '" width="100" height="100" class="img" alt="Generic placeholder thumbnail">');
+            });
+
+            //document.getElementById('status').innerHTML ='Thanks for logging in, ' + response.name + '!';
         });
     }
+
+   // var list = '<tr><td>' + + '</td><td>Lorem</td></tr>';
+
+    //$('#list').html(list);
+    $('#logout').click(function() {
+        FB.logout(function(response) {
+            if(window.location.pathname == '/project/index.html') {
+                window.location.href = 'login.html';
+            }
+            console.log('logged out ' + response);
+        });
+    });
+
+    $('#submit').click(function() {
+
+      //var title = <?php echo $_POST["title"]?;>;
+      //var description = <?php echo $_POST["description"]?>;
+      //var email = <?php echo $_POST["email"]?>;
+      //var contact = <?php echo $_POST["contact"]?>;
+
+      firebase.database().ref('services/' + userId).set({
+        title: title,
+        description: description,
+        email: email,
+        contact: contact
+      });
+    });
+
+    function writeUserData(userId, name) {
+      firebase.database().ref('user/' + userId).set({
+        name: name
+      });
+    }
+
+    $('#addService').click(function() {
+      //firebase.database().ref('services/' + userId).set({
+      //  title: title 
+      //});
+    });
+});
+
